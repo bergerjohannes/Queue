d215cee93a3b49c241e3499e7500ec5fee6ec0c3
Johannes Berger <johannes@berger.ac>
2022-08-07 14:15:29 +0200

    Remove milliseconds from response JSON

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
 import datetime
 
 def get_readable_time_from_ingame_timestamp(ingame_time):
-    return str(datetime.timedelta(milliseconds=ingame_time))
+    return str(datetime.timedelta(milliseconds=ingame_time)).split('.')[0]
